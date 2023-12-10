f51b6c31f9f91ffe89a6a66c99bed9315416561b
Hassan Mourtada <hassanmourtada15>
2023-12-10 20:53:57 +0200

    NewDb

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize('fintech', 'root', '4B3VMbY94wgUk5sopV9F4NKtg3va6wog', {
-    dialect: 'PostgreSQL',
+    dialect: 'postgres',
     host: "postgres://root:4B3VMbY94wgUk5sopV9F4NKtg3va6wog@dpg-clr0ffae9h4c73ar3d60-a.oregon-postgres.render.com/fintech",
     port: '5432',
   
